48131107ec8fd7c6f9e3a4fcef2a7b2d3c8ed3eb
hunkxiaoye <yang3284627@vip.qq.com>
2018-04-27 23:23:52 +0800

    commit

--- src/main/java/com/bootdemo/Application.java
+++ src/main/java/com/bootdemo/Application.java
@@ -17,31 +17,31 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})//,VelocityAutoConfiguration.class
-@MapperScan("com.bootdemo.dao")
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+//@MapperScan("com.bootdemo.dao")
 @ImportResource(locations={"classpath:spring-mvc.xml","classpath:spring-mybatis.xml"})
 public class Application {
     private static Logger log = LogManager.getLogger(Application.class);
 
-    @Bean
-    @ConfigurationProperties(prefix="spring.datasource.bootdemo-db")
-    public DataSource dataSource() {
-        return new org.apache.tomcat.jdbc.pool.DataSource();
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml"));
-        return sqlSessionFactoryBean.getObject();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
+//    @Bean
+//    @ConfigurationProperties(prefix="spring.datasource.bootdemo-db")
+//    public DataSource dataSource() {
+//        return new org.apache.tomcat.jdbc.pool.DataSource();
+//    }
+//
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+//        sqlSessionFactoryBean.setDataSource(dataSource());
+//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml"));
+//        return sqlSessionFactoryBean.getObject();
+//    }
+//
+//    @Bean
+//    public PlatformTransactionManager transactionManager() {
+//        return new DataSourceTransactionManager(dataSource());
+//    }
 
 
     /**
